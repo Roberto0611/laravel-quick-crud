f3124dc1fc3dcf71867b8c944cb92767dfc9e1a6
Roberto0611 <ochoacuevasr@gmail.com>
2025-12-19 14:41:59 -0600

    first commit

---
+++ src/QuickCrudServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Roc0611\QuickCrud;
+
+use Illuminate\Support\ServiceProvider;
+
+class QuickCrudServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+       //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
